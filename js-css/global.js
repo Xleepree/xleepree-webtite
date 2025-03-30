cf91c35e84fb102d4c13fe1dfd46911b2845b169
Xleepree <diegoed1313@gmail.com>
2025-03-30 19:34:06 -0400

    funyubns
    crareca

--- js-css/global.js
+++ js-css/global.js
@@ -1,24 +1,18 @@
-console.log("No uses so far.")
-console.log("But just for fun:")
+// theme
 
-const funny = Number(3.1415926535897932384626433832795028419716939937510);
-const tips = ["follow", "somebody", "home"];
-const mrMcClackle = {}
+    function setTheme(theme) {
+        document.documentElement.classList.remove("lightTheme", "darkTheme");
+        if (theme) {
+            document.documentElement.classList.add(theme);
+        }
 
-mrMcClackle.peopleKilled = Number(3768420);
-mrMcClackle.childrenInBasement = Number(911000);
+        localStorage.setItem("theme", theme);
+    }
 
-console.log("Mr. McClackle has killed:")
-console.log(mrMcClackle.peopleKilled)
-console.log("people.")
-
-console.log("Mr. McClackle has:")
-console.log(mrMcClackle.childrenInBasement)
-console.log("children in his basement. His lair.")
-
-console.log("also, funny number:")
-console.log(funny)
-
-console.log("lastly, here's some tips for life:")
-console.log(tips)
+    document.addEventListener("DOMContentLoaded", () => {
+        const savedTheme = localStorage.getItem("theme") || "";
+        if (savedTheme) {
+            document.documentElement.classList.add(savedTheme);
+        }
+    });
 
