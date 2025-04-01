e20980dc9937091148d53cf9f6e208ef184df849
Xleepree <diegoed1313@gmail.com>
2025-04-01 15:04:30 -0400

    GAHAHA
    avbrile fooles

--- js-css/global.js
+++ js-css/global.js
@@ -1,6 +1,10 @@
 // theme
 
-    function setTheme(theme) {
+    function setTheme() {
+        alert("You want themes? I'm sorry, it's April Fools Day!")
+    }
+
+    /* function setTheme(theme) {
         document.documentElement.classList.remove("lightTheme", "darkTheme");
         if (theme) {
             document.documentElement.classList.add(theme);
@@ -14,5 +18,6 @@
         if (savedTheme) {
             document.documentElement.classList.add(savedTheme);
         }
-    });
+    }); */
 
+console.log("happy april fools day !!")
