60a18026dd78247785c138b5919b6f92ec9b92a3
Xleepree <diegoed1313@gmail.com>
2025-04-02 23:02:58 -0400

    existental crisis
    ohey ayh

--- js-css/global.js
+++ js-css/global.js
@@ -1,3 +1,8 @@
+let blogPost = false;
+if (window.location.pathname.includes("/blog-posts/")) {
+    blogPost = true;
+}
+
 // theme
 
     function setTheme(theme) {
@@ -16,4 +21,62 @@
         }
     });
 
-console.log("Hello.");
+// loading
+
+    // base
+
+        const banner = document.createElement("div");
+        banner.className = "bannerContainer";
+
+        const nav = document.createElement("div");
+        nav.className = "nav";
+
+        const footer = document.createElement("div");
+        footer.className = "footer";
+
+    // function
+
+        function loadContent(type) {
+            if (type === "page") {
+                banner.innerHTML = "<img src='images/banner.png' class='banner'>"; 
+                nav.innerHTML = `<a href="index.html">home</a>
+                    <a href="about.html">about</a>
+                    <a href="blog.html">blog</a>
+                    <a href="projects.html">projects</a>`;
+                footer.innerHTML = `<button onclick="setTheme('')">default</button>
+                    <button onclick="setTheme('lightTheme')">light</button>
+                    <button onclick="setTheme('darkTheme')">dark</button>
+                    <hr>
+                    <p>since nov 3, 2024 :: hosted with cloudflare</p>
+                    <a href="https://github.com/Xleepree/xleepree-webtite">repository</a> <a href="mailto:xleepree.pages.mail@gmail.com">contact</a>`;
+            } else if (type === "blogPost") {
+                banner.innerHTML = "<img src='../images/banner.png' class='banner'>";
+                nav.innerHTML = `<a href="../index.html">home</a>
+                <a href="../about.html">about</a>
+                <a href="../blog.html">blog</a>
+                <a href="../projects.html">projects</a>`;
+                footer.innerHTML = `<button onclick="setTheme('')">default</button>
+                    <button onclick="setTheme('lightTheme')">light</button>
+                    <button onclick="setTheme('darkTheme')">dark</button>
+                    <hr>
+                    <p>since nov 3, 2024 :: hosted with cloudflare</p>
+                    <a href="https://github.com/Xleepree/xleepree-webtite">repository</a> <a href="mailto:xleepree.pages.mail@gmail.com">contact</a>`;
+            } else {
+                console.log("unusable or nonexistent parameter.");
+                return;
+            }
+
+            document.body.prepend(nav);
+            document.body.prepend(banner);
+            document.body.append(footer);
+        }
+    
+    if (blogPost === true) {
+        document.addEventListener("DOMContentLoaded", () => {
+            loadContent('blogPost');
+        });
+    } else {
+        document.addEventListener("DOMContentLoaded", () => {
+            loadContent('page');
+        });
+    }
